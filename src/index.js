9dd00c22b67ae83181782cdc56828b67b89298d3
aquazor <kornevskiy99@gmail.com>
2023-01-02 17:10:15 +0200

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let reversed = "";
+    let strN = Math.abs(n).toString();
+
+    for (let i = 0; i < strN.length; i++) {
+        if (strN[strN.length - 1] === "0") {
+            strN = strN.slice(0, -1);
+        }
+        reversed = strN[i] + reversed;
+        // reversed = '1' + '' , = '2' + '1' , = '3' + '21'
+    }
+    // console.log(reversed);
+    return reversed;
+};
